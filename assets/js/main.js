53c9999be1b043f390f9843f348d553eabe1233c
evelingcastillon <evelingcastillon0304@gmail.com>
2021-05-05 00:19:11 +0200

    aggiunto - evenListener

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,34 @@
-//Nome Utente
-var inputElement = document.getElementById("nome_utente");
-inputElement.value = "Pippo";
 
-//km
-var kmElement = document.getElementById("km");
-kmElement.value = "20";
 
 //Codice treno (numero casuale tra 90000 e 100000 escluso)
-var numeroRandom = Math.floor(Math.random() * 99999) + 90000;
-document.getElementById("codice_treno").innerHTML = numeroRandom;
+
+
 //Numero carrozza
 
-//Prezzo calcolato
+/* Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km) */
+
+
+// CALCOLO BIGLIETTO
+
+var generaBigliettoEl = document.getElementById('genera_biglietto');
+
+generaBigliettoEl.addEventListener('click', function () {
+    
+    //Nome Utente
+    var inputElement = document.getElementById("nome_utente");
+    document.getElementById('nome_biglietto').innerHTML = inputElement.value
+    //km
+    var kmElement = document.getElementById("km");
+    //eta
+    var etaUtenteElement = document.getElementById('eta_utente');
+
+    var numeroRandom = Math.floor(Math.random() * 99999) + 90000;
+    document.getElementById("codice_treno").innerHTML = numeroRandom;
+    
+
+
+    var prezzoElement = document.getElementById("prezzo");
+    var prezzoPerChilometro = kmElement.value * 0.21;
+
+});
